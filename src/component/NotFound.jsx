6dbd4e42870da45f6a676f3b81f75c730ed988cf
Manoj Roy <greatsoul37642@gmail.com>
2025-01-14 20:05:08 +0530

    finish

--- src/component/NotFound.jsx
+++ src/component/NotFound.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/"); // Navigates to the About page
+  };
   return (
-    <div className="w-full h-screen flex items-center justify-center text-sm sm:text-xl md:text-2xl">
-      NotFound
+    <div className="w-full h-screen flex items-center justify-center text-sm sm:text-xl md:text-2xl flex-col">
+      <div> NotFound</div>
+      <div>
+        <button
+          onClick={handleNavigate}
+          className={`text-sm px-2 py-1 border border-gray-500 rounded-md`}
+        >
+          go back
+        </button>
+      </div>
     </div>
   );
 }
