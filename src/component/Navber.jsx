c4755e67147b6ca2e71825061d61e4f8d39c73dd
Manoj Roy <greatsoul37642@gmail.com>
2025-01-14 08:44:18 +0530

    add nav items

---
+++ src/component/Navber.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { MdWbSunny } from "react-icons/md";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+
+function Navber() {
+  return (
+    <div className="flex items-center justify-items-end">
+      <MdWbSunny />
+      <BsFillMoonStarsFill />
+    </div>
+  );
+}
+
+export default Navber;
